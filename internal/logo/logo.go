fe27760aae635bcaef48d6a214755772e1504432
Parham Alvani <parham.alvani@gmail.com>
2025-11-24 03:53:16 +0000

    fix: correct lint issues

--- internal/logo/logo.go
+++ internal/logo/logo.go
@@ -7,7 +7,6 @@ import (
 
 // Print displays the colorful Deities logo.
 func Print() {
-	// Create a big text with gradient colors
 	s, _ := pterm.DefaultBigText.WithLetters(
 		putils.LettersFromStringWithStyle("D", pterm.NewStyle(pterm.FgCyan)),
 		putils.LettersFromStringWithStyle("eities", pterm.NewStyle(pterm.FgLightBlue)),
@@ -15,8 +14,8 @@ func Print() {
 
 	pterm.DefaultCenter.Println(s)
 
-	// Print subtitle
 	subtitle := pterm.DefaultCenter.Sprint("Kubernetes Image Digest Monitor")
+
 	pterm.Println()
 	pterm.Println(pterm.LightBlue(subtitle))
 	pterm.Println()
